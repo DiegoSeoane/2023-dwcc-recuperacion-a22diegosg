1843b72d6fa74042ad801f794044cab824f3e140
ilizor <diegoseoges@gmail.com>
2024-04-20 10:54:41 +0200

    randomchoise acabado

--- ProxectosAdicionais/RandomChoicePicker/script.js
+++ ProxectosAdicionais/RandomChoicePicker/script.js
@@ -1,7 +1,39 @@
-let texto = document.querySelector('#textarea');
+let textarea = document.querySelector('#textarea');
 
-texto.addEventListener('input',(event)=>{
-    if (event.target.value === ',') {
-        console.log('a');
+let divTags = document.querySelector('#tags');
+
+textarea.addEventListener('keyup', (event) => {
+
+    let arraytexto = event.target.value.split(',');
+
+    divTags.innerHTML = ''
+    for (const tag of arraytexto) {
+        let span = document.createElement('span');
+        span.innerHTML = tag;
+        span.classList.add('tag');
+        divTags.append(span);
+    }
+
+    if (event.key === 'Enter') {
+        setTimeout(() => {
+            event.target.value = '';
+        }, 10);
+        let intervalo = setInterval(() => {
+            let tags = document.querySelectorAll('.tag');
+            let randomNumber = Math.floor(Math.random() * tags.length);
+            tags[randomNumber].classList.add('highlight');
+            setTimeout(() => {
+                tags[randomNumber].classList.remove('highlight')
+            }, 200);
+        }, 200);
+
+        setTimeout(() => {
+            clearInterval(intervalo);
+            setTimeout(() => {
+                let tags = document.querySelectorAll('.tag');
+                let randomNumber = Math.floor(Math.random() * tags.length);
+                tags[randomNumber].classList.add('highlight');
+            }, 200)
+        }, 3000)
     }
 })
